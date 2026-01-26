ae7682ac65f1bf65a6dc3609b9971fe1307a15de
Don Maslanka <don.maslanka@toptal.com>
2026-01-25 17:56:43 -0700

    Add Katalon project files

---
+++ Test Suites/Smoke.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>abcd1234-5678-90ab-cdef-1234567890ab</testSuiteGuid>
+   <testCaseLink></testCaseLink>
+</TestSuiteEntity>
